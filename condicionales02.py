c515da0f0606319abefb880517a32cc9722ac6bb
Otero1407 <145025813+Otero1407@users.noreply.github.com>
2023-09-26 15:47:46 -0600

    Create condicionales02.py

---
+++ condicionales02.py
@@ -0,0 +1,7 @@
+num=float(input("inserte cualquier nunmero: "))
+if num < 0:
+  print("el número es negativo")
+elif num == 0:
+  print("el número es cero")
+elif num > 0:
+  print("el número es positivo")
